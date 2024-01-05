8990589964d4dd3fd21ae1d7ff9d7fb2b5da3e5e
karolinajj <115790529+karolinajj@users.noreply.github.com>
2024-01-05 21:12:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showDiv(value) {
+    var Div1 = document.getElementById("sourceDiv");
+    var Div2 = document.getElementById("servingsDiv");
+    var Div3 = document.getElementById("ingredientsDiv");
+    if (value.trim() !== "") {
+        Div1.style.display = "block";
+        Div2.style.display = "block";
+        Div3.style.display = "block";
+    } else {
+        Div1.style.display = "none";
+        Div2.style.display = "none";
+        Div3.style.display = "none";
+    }
+}
