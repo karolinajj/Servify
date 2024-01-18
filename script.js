27b3cbc1bb99641cad9186db77ddad5fc2830e99
karolinajj <115790529+karolinajj@users.noreply.github.com>
2024-01-18 19:15:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -69,8 +69,8 @@ function showServingsButtons(evt) {
 }
 
 async function calculateServings(servings) {
-    Div4.style.display = "block";
     Div4.innerHTML = '';
+    Div4.style.display = "block";
     await getDataFromAniaGotuje("", "full" )
     for(let i = 0; i < recipeFull.length; i++){
         let lineDiv = document.createElement("div");
