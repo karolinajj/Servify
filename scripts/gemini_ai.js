521fba7aeff6944ccc9d87cf8cc9667d9447a338
karolinajj <115790529+karolinajj@users.noreply.github.com>
2024-06-01 19:19:59 +0200

    Add files via upload

--- scripts/gemini_ai.js
+++ scripts/gemini_ai.js
@@ -5,7 +5,7 @@ function parsedResponse(text){
 async function getResponseFromGeminiAI(recipeServingsOrginal, servings, recipe, recipeName){
   
   const prompt1 = "Jestem kucharzem i mam oryginalny przepis na " + recipeName + " dla " + recipeServingsOrginal + " osób.";
-  const prompt2 = "Proszę przelicz składniki tego przepisu, aby można było przygotować potrawę dla " + servings + " osób. Odpowiedź zapisz w formacie: * Przepis dla ... * składnik 1 - ilość 1 * składnik 2 - ilość 2 * Krok 1 * Krok 2 * itd. W pierwszej linii napisz, dla ilu osób przeliczono przepis (Przepis dla ... osób). Następnie wygeneruj przepis na " + recipeName + " z wykorzystaniem obliczonych składników. Koniecznie użyj podanego formatu odpowiedzi (każdą nową linię oddzielaj znakiem * ), pamiętaj o wygenerowaniu kroków do wykonania przepisu.";
+  const prompt2 = "Proszę przelicz składniki tego przepisu, aby można było przygotować potrawę dla " + servings + " osób. Odpowiedź zapisz w formacie: * Przepis dla ... * składnik 1 - ilość 1 * składnik 2 - ilość 2 * Wykonanie: * Krok 1 * Krok 2 * itd. W pierwszej linii napisz, dla ilu osób przeliczono przepis (Przepis dla ... osób). Następnie wygeneruj przepis na " + recipeName + " z wykorzystaniem obliczonych składników. Po podaniu składników napisz w linii '* Wykonanie *' (Zwróć uwagę na to, żeby wystąpiły znaki *). Koniecznie użyj podanego formatu odpowiedzi (każdą nową linię oddzielaj znakiem * ), pamiętaj o wygenerowaniu kroków do wykonania przepisu.";
   const prompt3 = "Oto składniki: ";
   const result = await model.generateContent(prompt1 + prompt2 + prompt3 + recipe);  
   const response = await result.response;
